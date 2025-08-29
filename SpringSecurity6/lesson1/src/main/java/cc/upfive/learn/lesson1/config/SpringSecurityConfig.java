d50e05e8ce4ff74289c8320e599c5342d9f870b8
fiveupup <163608524+fiveupup@users.noreply.github.com>
2025-08-29 15:25:43 +0800

    update

--- SpringSecurity6/lesson1/src/main/java/cc/upfive/learn/lesson1/config/SpringSecurityConfig.java
+++ SpringSecurity6/lesson1/src/main/java/cc/upfive/learn/lesson1/config/SpringSecurityConfig.java
@@ -27,7 +27,7 @@ public class SpringSecurityConfig {
     @Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login").permitAll()
+//                        .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
