a0b309d2178baaa5c5ce9441d8337533ec1ddf05
fiveupup <163608524+fiveupup@users.noreply.github.com>
2025-09-03 17:42:31 +0800

    update

--- SpringSecurity6/lesson0/sso-client/src/main/java/cc/upfive/learn/lesson0/SecurityConfig.java
+++ SpringSecurity6/lesson0/sso-client/src/main/java/cc/upfive/learn/lesson0/SecurityConfig.java
@@ -1,8 +1,9 @@
 package cc.upfive.learn.lesson0;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,6 +11,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,10 +27,7 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/user", true)
                 )
                 .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
+                        .logoutSuccessUrl("/")
                 )
         ;
         return http.build();
